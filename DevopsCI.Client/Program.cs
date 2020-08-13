550678323002ca60b639754b7fae937c52c41851
Carlos Chavez <cvchavez92@gmail.com>
2020-08-13 15:27:50 -0600

    added package to ci pipeline

--- DevopsCI.Client/Program.cs
+++ DevopsCI.Client/Program.cs
@@ -9,18 +9,19 @@ using Microsoft.Extensions.Logging;
 
 namespace DevopsCI.Client
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseUrls("http://*:8080");
-                });
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+                  // webBuilder.UseStartup<Startup>().UseUrls("http://*:8080");
+                });
+  }
 }
--- dockerfile
+++ dockerfile
@@ -1,6 +1,13 @@
-FROM mcr.microsoft.com/dotnet/core/sdk
+FROM mcr.microsoft.com/dotnet/core/sdk as publish
 WORKDIR /app
-COPY . /app 
+# COPY . /app 
+COPY . .
+RUN dotnet publish -c Release -o out DevopsCI.Client/DevopsCI.Client.csproj
+
+FROM mcr.microsoft.com/dotnet/core/aspnet
+WORKDIR /dist
+COPY --from=publish /app/out /dist
 # or COPY . .  take all the projects in current directory to the image environment where I am currently in
 # ENV ASPNETCORE_URLS='http://*:5000'
-CMD [ "dotnet", "run", "-p", "DevopsCI.Client/DevopsCI.Client.csproj" ]
+CMD [ "dotnet", "DevopsCI.Client.dll" ]
+
